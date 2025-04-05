f2a01f8418a957d860289379f61eb0240836e3e9
Hrita-Dutta <hritadutta@gmail.com>
2025-04-05 19:03:05 +0545

    resetting the game

--- script.js
+++ script.js
@@ -12,16 +12,30 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
+let scores, currentScore, activePlayer, playing;
+
 // Starting conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
 
+const init = function(){
+    
+    
+    scores = [0, 0];
+    currentScore = 0;
+    activePlayer = 0;
+    playing = true;
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+    score0El.textContent = 0;
+    score1El.textContent = 0;
+    current0El.textContent = 0;
+    current1El.textContent = 0;
+
+    diceEl.classList.add('hidden');
+    player0El.classList.remove('player--winner');
+    player1El.classList.remove('player--winner');
+    player0El.classList.add('player--active');
+    player1El.classList.remove('player--active');
+};
+init();
 
 const switchPlayer = function(){
     document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -35,7 +49,7 @@ btnRoll.addEventListener('click', function(){
     if(playing){
     // 1. Generating a random dice roll
     const dice = Math.trunc(Math.random() * 6) + 1;
-    console.log(dice);
+    
     
 
     // 2. Display dice
@@ -60,7 +74,6 @@ btnHold.addEventListener('click', function(){
     if(playing){
     // 1. Add current score to active player's score
     scores[activePlayer] +=currentScore;    // scores[1] = scores[1] + currentScore
-    console.log(scores[activePlayer]);
     document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
 
     // 2. Check if player's score is >= 100
@@ -77,3 +90,5 @@ btnHold.addEventListener('click', function(){
     }
     }
 });
+
+btnNew.addEventListener('click', init);
